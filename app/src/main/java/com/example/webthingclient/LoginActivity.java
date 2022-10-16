c0b2ac219101d69ed5050207f7b5cc2edd4b8ed5
aqdamzain <aqdam.zain@ui.ac.id>
2022-10-16 14:47:08 +0700

    binding main, Login

--- app/src/main/java/com/example/webthingclient/LoginActivity.java
+++ app/src/main/java/com/example/webthingclient/LoginActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.webthingclient.databinding.ActivityLoginBinding;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
